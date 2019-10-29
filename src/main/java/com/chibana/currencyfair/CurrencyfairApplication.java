1f8034e6f354d682cb2883400ca1ebc33c0855ad
Rodrigo Chibana <rodrigo.chibana@gmail.com>
2019-10-29 10:18:05 +0000

    Initial commit

---
+++ src/main/java/com/chibana/currencyfair/CurrencyfairApplication.java
@@ -0,0 +1,13 @@
+package com.chibana.currencyfair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyfairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyfairApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chibana/currencyfair/CurrencyfairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chibana.currencyfair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyfairApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
