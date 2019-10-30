71253d70b3a968b07add300667e47e2fea5dc180
Rodrigo Chibana <rodrigo.chibana@gmail.com>
2019-10-30 09:26:03 +0000

    Adding some data constraints

--- src/main/java/com/chibana/currencyfair/model/Transaction.java
+++ src/main/java/com/chibana/currencyfair/model/Transaction.java
@@ -1,11 +1,16 @@
 package com.chibana.currencyfair.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 import java.util.Calendar;
 
@@ -15,19 +20,49 @@ import java.util.Calendar;
  **/
 
 @Data
+@NoArgsConstructor
 @Entity
 public class Transaction {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @PositiveOrZero
+    @NotNull
     private Long userId;
+
+    @NotNull
     private String currencyFrom;
+
+    @NotNull
     private String currencyTo;
+
+    @Positive
     private BigDecimal amountSell;
+
+    @Positive
     private BigDecimal amountBuy;
+
+    @Positive
     private Double rate;
+
+    @NotNull
     private Calendar timePlaced;
+
+    @NotNull
     private String originatingCountry;
 
+    public Transaction(Long userId, String currencyFrom, String currencyTo, BigDecimal amountSell, BigDecimal amountBuy,
+                       Double rate, Calendar timePlaced, String originatingCountry) {
+
+        this.userId = userId;
+        this.currencyFrom = currencyFrom;
+        this.currencyTo = currencyTo;
+        this.amountSell = amountSell;
+        this.amountBuy = amountBuy;
+        this.rate = rate;
+        this.timePlaced = timePlaced;
+        this.originatingCountry = originatingCountry;
+    }
 }
