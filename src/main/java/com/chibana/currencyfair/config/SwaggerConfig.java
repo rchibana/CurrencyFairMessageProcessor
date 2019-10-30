3708b28541478d81b1c74ee986fbcb8ad0a30500
Rodrigo Chibana <rodrigo.chibana@gmail.com>
2019-10-30 22:06:10 +0000

    Adding swagger to our application

---
+++ src/main/java/com/chibana/currencyfair/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.chibana.currencyfair.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by Rodrigo Chibana
+ * Date: 30/10/2019
+ **/
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.chibana.currencyfair.controller"))
+                .build();
+    }
+
+}
